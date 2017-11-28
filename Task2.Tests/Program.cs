a4e3d0f4ca692ac5abc4d1bc5625a350c069555d
Yuliya Shakhrai <32871561+YuliyaShakhrai@users.noreply.github.com>
2017-11-28 09:47:56 +0300

    Add files via upload

---
+++ Task2.Tests/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var byteGen = new RandomBytesFileGenerator();
+            var charGen = new RandomCharsFileGenerator();
+
+            byteGen.GenerateFiles(2, 10);
+            charGen.GenerateFiles(2, 10);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task2/IFileGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public interface IFileGenerator
+    {
+        string WorkingDirectory { get; }
+        string FileExtension { get; }
+
+        void GenerateFiles(int filesCount, int contentLength);
+        byte[] GenerateFileContent(int contentLength);
+        void WriteBytesToFile(string fileName, byte[] content);
+
+    }
+}
