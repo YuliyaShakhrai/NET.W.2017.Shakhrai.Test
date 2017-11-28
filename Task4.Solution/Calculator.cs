a31fb902c0e8c8255b23b02ae08b46d1bdedc4d5
Yuliya Shakhrai <32871561+YuliyaShakhrai@users.noreply.github.com>
2017-11-28 10:48:30 +0300

    Add files via upload

---
+++ Task4.Solution/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task4
+{
+    public class Calculator
+    {
+        public double CalculateAverage(List<double> values, IAveragingMethod averagingMethod)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException();
+            }
+            return averagingMethod.CalculateAverage(values);
+        }
+    }
+}
---
+++ Task4.Solution/MeanAveragingMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class MeanAveragingMethod : IAveragingMethod
+    {
+        public double CalculateAverage(List<double> values)
+        {
+            return values.Sum() / values.Count;
+        }
+    }
+}
