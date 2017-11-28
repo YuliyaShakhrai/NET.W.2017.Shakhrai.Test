69f79517179ba368c0eed8f6aa4cd7ce675e65eb
Yuliya Shakhrai <32871561+YuliyaShakhrai@users.noreply.github.com>
2017-11-28 09:29:24 +0300

    Add files via upload

---
+++ Task1.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Task1.Console
+{
+    using System;
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var repository = new SqlRepository();
+            var verifier = new PasswordVerifier();
+            var service = new PasswordCheckerService(repository, verifier);
+
+            var pass1 = "1234567";
+            var pass2 = "abcdefg";
+            var pass3 = "sdadkjwld8732309!";
+            var pass4 = "1111111aaa";
+
+            string[] passwords = { pass1, pass2, pass3, pass4 };
+
+            foreach (var pass in passwords)
+            {
+                Console.WriteLine("{0} {1} {2}", pass, service.CheckPassword(pass).Item1, service.CheckPassword(pass).Item2);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task1/PasswordCheckerService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1
+{
+    public class PasswordCheckerService
+    {
+        private IRepository _repository;
+        private IPasswordVerifier _verifier;
+
+        public PasswordCheckerService(IRepository repository, IPasswordVerifier verifier)
+        {
+            this.Repository = repository;
+            this.PasswordVerifier = verifier;
+        }
+
+        public IRepository Repository {
+            get
+            {
+                return _repository;
+            }
+            set
+            {
+                _repository = value;
+            }
+        }
+
+        public IPasswordVerifier PasswordVerifier
+        {
+            get
+            {
+                return _verifier;
+            }
+            set
+            {
+                _verifier = value;
+            }
+        }
+
+        public Tuple<bool, string> CheckPassword(string password)
+        {
+            var result = PasswordVerifier.VerifyPassword(password);
+            if (result.Item1 == true)
+            {
+                Repository.Create(password);
+            }
+            return result;
+        }
+    }
+}
