6448535ae4421949e0e00e5b2524ab0bee47c9cc
Yuliya Shakhrai <32871561+YuliyaShakhrai@users.noreply.github.com>
2017-11-29 19:11:50 +0300

    Added tests for Task1 and Task5
    Added tests for Task1 and Task5

---
+++ Task1.Solution.Tests/PasswordCheckerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Task1.Solution.Tests
+{
+    [TestFixture]
+    public class PasswordCheckerTests
+    {
+        [TestCase("123", false)]
+        [TestCase("sss1", false)]
+        [TestCase("", false)]
+        [TestCase("1222111222112221212", false)]
+        [TestCase("asdf1234sa", true)]
+        [TestCase("1qqqqqeeee", true)]
+        [TestCase("fdsfsdfsdfa12324234sdsdasdas", false)]
+        public void VerifyPasswordTests(string password, bool expected)
+        {
+            var repository = new SqlRepository();
+            var passwordCheckerService = new PasswordCheckerService(repository);
+            var verifier =
+                new PasswordVerifierBuilder().WithNoEmpty().WithMinLength(5).WithMaxLength(12).WithOneDigit().WithOneLetter().Build();
+
+            var result = passwordCheckerService.CheckPassword(password, verifier);
+
+            Assert.AreEqual(expected, result.success);
+        }
+
+
+    }
+}
---
+++ Task5.Tests/Task5SolutionTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Task5.Solution;
+using NUnit.Framework;
+
+
+namespace Task5.Tests
+{
+    [TestFixture]
+
+    public class Task5SolutionTests
+    {
+        [Test]
+        public void HTMLConverter_Test()
+        {
+            List<DocumentPart> parts = new List<DocumentPart>
+                {
+                    new PlainText {Text = "Some plain text"},
+                    new Hyperlink {Text = "google.com", Url = "https://www.google.by/"},
+                    new BoldText {Text = "Some bold text"}
+                };
+            Document document = new Document(parts);
+            var htmlConverter = new HTMLConverter();
+
+            var expected = "Some plain text\r\n<a href=\"https://www.google.by/\">google.com</a>\r\n<b>Some bold text</b>\r\n";
+
+            Assert.AreEqual(expected, (string)document.Convert(htmlConverter));
+        }
+
+        [Test]
+        public void PlainConverter_Test()
+        {
+            List<DocumentPart> parts = new List<DocumentPart>
+                {
+                    new PlainText {Text = "Some plain text"},
+                    new Hyperlink {Text = "google.com", Url = "https://www.google.by/"},
+                    new BoldText {Text = "Some bold text"}
+                };
+            Document document = new Document(parts);
+            var plainConverter = new PlainTextConverter();
+
+            var expected = "**Some plain text**\r\ngoogle.com [https://www.google.by/]\r\nSome bold text\r\n";
+
+            Assert.AreEqual(expected, (string)document.Convert(plainConverter));
+        }
+
+        [Test]
+        public void LaTeXConverter_Test()
+        {
+            List<DocumentPart> parts = new List<DocumentPart>
+                {
+                    new PlainText {Text = "Some plain text"},
+                    new Hyperlink {Text = "google.com", Url = "https://www.google.by/"},
+                    new BoldText {Text = "Some bold text"}
+                };
+            Document document = new Document(parts);
+            var latexConverter = new LaTeXConverter();
+
+            var expected = "Some plain text\r\n\\href{https://www.google.by/}{google.com}\r\n\\textbf{Some bold text}\r\n";
+
+            Assert.AreEqual(expected, (string)document.Convert(latexConverter));
+        }
+
+
+
+    }
+}
